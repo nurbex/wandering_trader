4fe4a6c93f79fcf8059a6cdacc8745de7b47ac59
Nurbek <nurbekaj@gmail.com>
2021-06-25 18:21:33 -0500

    Creating project task manager. Task domain was created.

---
+++ src/main/java/com/wandering/trader/domain/Task.java
@@ -0,0 +1,80 @@
+package com.wandering.trader.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private Long id;
+    private String task;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public enum STATUS {
+        IDEA,
+        BUG,
+        ISSUE,
+        FEATURE,
+        ENHANCEMENT,
+        THISWEEK,
+        TODAY,
+        TEST,
+        DONE
+    };
+
+    private STATUS status;
+
+    @ManyToOne
+    CustomUser owner;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public STATUS getStatus() {
+        return status;
+    }
+
+    public void setStatus(STATUS status) {
+        this.status = status;
+    }
+
+    public CustomUser getOwner() {
+        return owner;
+    }
+
+    public void setOwner(CustomUser owner) {
+        this.owner = owner;
+    }
+}
